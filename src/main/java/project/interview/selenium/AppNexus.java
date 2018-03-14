7c1ff2ecfac78dfe3cb4de71184f897fe9101f81
karthickupgraded@gmail.com <karthickupgraded@gmail.com>
2018-03-14 10:57:20 +0530

    finding a way to automate newtwork logs of browsers, appnexus

--- src/main/java/project/interview/selenium/AppNexus.java
+++ src/main/java/project/interview/selenium/AppNexus.java
@@ -1,4 +1,5 @@
 package project.interview.selenium;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -7,6 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 import org.apache.http.HttpResponse;
@@ -14,7 +16,12 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -26,11 +33,17 @@ import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.google.common.base.Function;
 
 public class AppNexus {
 	WebDriver driver = null;
@@ -82,25 +95,24 @@ public class AppNexus {
 		}
 	}
 
-	/*@Test(enabled = false)
-	public void getJSErrors() throws Exception {
-		// System.setProperty("webdriver.gecko.driver", "C:\\Program
-		// Files\\Mozilla Firefox\\firefox.exe");
-		
-		 * FirefoxBinary bin = new FirefoxBinary(new
-		 * File("C:\\Program Files\\Mozilla Firefox\\firefox.exe"));
-		 * FirefoxProfile firefoxProfile = new FirefoxProfile();
-		 
-		FirefoxProfile ffProfile = new FirefoxProfile();
-
-		final WebDriver driver = new FirefoxDriver(ffProfile);
-		driver.get("http://www.javascriptkit.com/javatutors/errortest.htm");
-		driver.switchTo().alert().dismiss();
-		Thread.sleep(2000L);
-		JavaScriptError.addExtension(ffProfile);
-		final List<JavaScriptError> jsErrors = JavaScriptError.readErrors(driver);
-		System.out.println("JS Errors: " + jsErrors.size());
-	}*/
+	/*
+	 * @Test(enabled = false) public void getJSErrors() throws Exception { //
+	 * System.setProperty("webdriver.gecko.driver", "C:\\Program //
+	 * Files\\Mozilla Firefox\\firefox.exe");
+	 * 
+	 * FirefoxBinary bin = new FirefoxBinary(new
+	 * File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")); FirefoxProfile
+	 * firefoxProfile = new FirefoxProfile();
+	 * 
+	 * FirefoxProfile ffProfile = new FirefoxProfile();
+	 * 
+	 * final WebDriver driver = new FirefoxDriver(ffProfile);
+	 * driver.get("http://www.javascriptkit.com/javatutors/errortest.htm");
+	 * driver.switchTo().alert().dismiss(); Thread.sleep(2000L);
+	 * JavaScriptError.addExtension(ffProfile); final List<JavaScriptError>
+	 * jsErrors = JavaScriptError.readErrors(driver);
+	 * System.out.println("JS Errors: " + jsErrors.size()); }
+	 */
 
 	@BeforeTest
 	@Parameters(value = { "browser" })
@@ -108,20 +120,21 @@ public class AppNexus {
 		DesiredCapabilities caps = null;
 		if (value.equals("chrome")) {
 			System.out.println("Chrome constructed");
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\MyDrivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\MyDrivers\\chromedriver.exe");
 			caps = DesiredCapabilities.chrome();
-			getLogs(caps);
+			//getLogs(caps);
 			driver = new ChromeDriver(caps);
 		} else if (value.equals("firefox")) {
 			System.out.println("Firefox constructed");
 			caps = DesiredCapabilities.firefox();
-			getLogs(caps);
+			//getLogs(caps);
 			driver = new FirefoxDriver(caps);
 		} else if (value.equals("ie")) {
 			System.out.println("IE constructed");
 			System.setProperty("webdriver.ie.driver", "D:\\Softwares\\IEDriverServer.exe");
 			caps = DesiredCapabilities.internetExplorer();
-			getLogs(caps);
+			//getLogs(caps);
 			driver = new InternetExplorerDriver(caps);
 		} else {
 			Assert.assertTrue(false, "Invalid browser");
@@ -139,18 +152,25 @@ public class AppNexus {
 		System.out.println("Test what:" + value);
 		LogEntries logEntries = driver.manage().logs().get(LogType.PERFORMANCE);
 		if (value.equals("chrome")) {
-			/*for (LogEntry entry : logEntries) {
-				//System.out.println("JS Error: " + entry.getMessage());
-				BufferedWriter bw = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+"\\logs.txt"));
-				bw.write(entry.getMessage());
-			}*/
-			/*BufferedWriter bw = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+"\\logs.txt"));
-			bw.write(logEntries.getAll().toString());*/
-			Assert.assertTrue(logEntries.getAll().toString().contains("https://ib.adnxs.com/getuid?https://anmap.pch.com/appnexus_getuid_postback?appnxs_uid=$UID"));
-			System.out.println("Passed");
+			/*
+			 * for (LogEntry entry : logEntries) {
+			 * //System.out.println("JS Error: " + entry.getMessage());
+			 * BufferedWriter bw = new BufferedWriter(new
+			 * FileWriter(System.getProperty("user.dir")+"\\logs.txt"));
+			 * bw.write(entry.getMessage()); }
+			 */
+			BufferedWriter bw = new BufferedWriter(new FileWriter(System.getProperty("user.dir") + "\\logs.txt"));
+			bw.write(logEntries.getAll().toString());
+			/*
+			 * Assert.assertTrue(logEntries.getAll().toString().contains(
+			 * "https://ib.adnxs.com/getuid?https://anmap.pch.com/appnexus_getuid_postback?appnxs_uid=$UID"
+			 * )); System.out.println("Passed");
+			 */
+			bw.close();
 		} else if (value.equals("firefox")) {
 			for (int i = 0; i < logEntries.getAll().size(); i++) {
-				System.out.println("JS Error: " + logEntries.getAll().get(logEntries.getAll().size() - i - 1).getMessage());
+				System.out.println(
+						"JS Error: " + logEntries.getAll().get(logEntries.getAll().size() - i - 1).getMessage());
 			}
 		} else {
 			Assert.assertTrue(false, "Browser Doesn't matched");
@@ -158,16 +178,103 @@ public class AppNexus {
 
 	}
 
+	public WebElement getElement(String locator) throws Exception {
+		Object obj = getElement(locator, 30);
+		if (obj != null)
+			return (WebElement) obj;
+		else
+			return null;
+	}
+
+	public WebElement getElement(String locator, int timeUnit) throws Exception {
+		return _getWebDriverWait(timeUnit, locator);
+	}
+
+	public List<WebElement> getElements(String locator) throws Exception {
+		return driver.findElements(By.cssSelector(locator));
+	}
+
+	public void waitForPageToLoad() throws Exception {
+		((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+	}
+
+	public void _getImplicitWait(int timeUnit) throws Exception {
+		driver.manage().timeouts().implicitlyWait(timeUnit, TimeUnit.SECONDS);
+	}
+
+	public WebElement _getWebDriverWait(int timeUnit, String locator) throws Exception {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, timeUnit);
+			return wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(locator)));
+		} catch (TimeoutException e) {
+			return null;
+		}
+	}
+
+	public WebElement _getFluentWait(int timeUnit, final String locator) throws Exception {
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(timeUnit, TimeUnit.SECONDS)
+				.pollingEvery(2, TimeUnit.SECONDS).ignoring(NoSuchElementException.class);
+		return wait.until(new Function<WebDriver, WebElement>() {
+			public WebElement apply(WebDriver driver) {
+				return driver.findElement(By.cssSelector(locator));
+			}
+		});
+	}
+
+	public void clickElement(String locator) throws Exception {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(locator))).click();
+			}
+			catch(TimeoutException e) {
+				System.out.println("Timeout exception");
+				throw new RuntimeException("Element not clickable");
+			}
+			catch(Exception e){
+				System.out.println("Exception happened");
+				throw new Exception("Exception happened");
+			}
+	}
+	
+	public void clickElement(WebElement element) throws Exception {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+			}
+			catch(TimeoutException e) {
+				System.out.println("Timeout exception");
+				throw new RuntimeException("Element not clickable");
+			}
+			catch(Exception e){
+				System.out.println("Exception happened");
+				throw new Exception("Exception happened");
+			}
+	}
+	
+	protected void clickElementJS(String locator) throws Exception {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click()", getElement(locator));
+	}
+	String clientProfiles = "[role='row'] [role='gridcell']:nth-child(2)";
+	
+	public void clickProfile(int index) throws Exception{
+		List<WebElement> items = getElements(clientProfiles);
+		clickElement(items.get(index));
+	}
+
 	@Test
 	@Parameters(value = { "browser" })
-	public void testMethod(String browser) throws InterruptedException, IOException {
-		String url = "https://lottods.qa.pch.com";
-		//String url = "http://www.rferl.org/";
+	public void testMethod(String browser) throws Exception {
+		String url = "http://52.57.28.184/authentication";
 		driver.get(url);
-		//driver.switchTo().alert().dismiss();
 		driver.manage().window().maximize();
-		Thread.sleep(2000);
-		analyzeLog(browser, url);
+		getElement("#username").sendKeys("lmesuperadmin@databp.com");
+		getElement("#password").sendKeys("databp#123");
+		clickElementJS("[type='submit']");
+		Thread.sleep(5000);
+		clickProfile(1);
+		System.out.println("Clicked");
+		Thread.sleep(5000);
 	}
 
 	@AfterTest
